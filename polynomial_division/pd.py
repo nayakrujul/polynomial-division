6ce9c9d590f526ff52ee6e8104b0708926531a0a
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-09-10 16:47:24 +0100

    Update pd.py

--- polynomial_division/pd.py
+++ polynomial_division/pd.py
@@ -1,3 +1,6 @@
+import argparse
+import ast
+
 def polynomial_divide(dividend, divisor):
     if len(divisor) >= len(dividend):
         raise ValueError('divisor bigger than dividend')
@@ -16,3 +19,16 @@ def polynomial_divide(dividend, divisor):
             pass
         d = _d
     return r
+
+def cmdline():
+    parser = argparse.ArgumentParser(prog ='divide',
+                                     description ='Divide polynomials: use the divide command')
+    parser.add_argument('dividend', metavar ='dividend', type=str, nargs=1, help= 'Dividend, like 2,-1,7,4')
+    parser.add_argument('divisor', metavar ='divisor', type=str, nargs=1, help= 'Divisor, like 2,1')
+    args = parser.parse_args()
+    try:
+        print("Result:", *polynomial_divide(ast.literal_eval(args.dividend[0]), ast.literal_eval(args.divisor[0])))
+        return 0
+    except:
+        print("ERROR")
+        return 1
